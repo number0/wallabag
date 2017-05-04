b60a70c8463244a1a0b146c0816a6502697c732f
Jeremy Benoist <jeremy.benoist@gmail.com>
2017-05-04 07:47:52 +0200

    Do not clean fixtures
    to avoid SQLite to re-use id for entry tag relation 😓

--- tests/Wallabag/CoreBundle/Controller/EntryControllerTest.php
+++ tests/Wallabag/CoreBundle/Controller/EntryControllerTest.php
@@ -1245,8 +1245,5 @@ class EntryControllerTest extends WallabagCoreTestCase
         $this->assertSame('Crimes et réformes aux Philippines', $content->getTitle());
 
         $client->getContainer()->get('craue_config')->set('restricted_access', 0);
-
-        $em->createQuery('DELETE FROM Wallabag\CoreBundle\Entity\SiteCredential s WHERE s.id = '.$credential->getId())->execute();
-        $em->createQuery('DELETE FROM Wallabag\CoreBundle\Entity\Entry e WHERE e.id = '.$content->getId())->execute();
     }
 }
